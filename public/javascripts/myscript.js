40c23d04a9bfb97b324cd380cf7f409ec269f6a2
Beaky <alema027@hotmail.fr>
2020-01-20 09:05:40 -0500

    Color coding

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -4,10 +4,10 @@ function postWOL() {
 	var xhttp = new XMLHttpRequest()
 	xhttp.onreadystatechange = function() {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
-			snackBar("WOL request sent !")
+			snackBar("WOL request sent !", false)
 		}
 		else{
-			snackBar("WOL request could not be sent.")
+			snackBar("WOL request could not be sent.", true)
 		}
 	}
 	xhttp.open('POST', '/WOL', true)
@@ -15,12 +15,19 @@ function postWOL() {
 
 }
 
-function snackBar(message) {
+function snackBar(message, err) {
 	// Get the snackbar DIV
 	var x = document.getElementById('snackbar')
 	x.innerHTML = message
 	// Add the "show" class to DIV
-	x.className = 'show'
+	x.className = 'show '
+	if(err){
+		x.className += 'snackError '
+	}
+	else{
+		x.className += 'snackSuccess '
+	}
+	
 	// After 3 seconds, remove the show class from DIV
 	setTimeout(function() {
 		x.className = x.className.replace('show', '')
